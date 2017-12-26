52a2a8d07355793c27294d8584b4d495f1376acc
southeast_px <southeast_px@163.com>
2017-12-26 10:06:43 +0800

    测试与github的连通性！

---
+++ src/cn/ksdshpx/test/TestGit.java
@@ -0,0 +1,13 @@
+package cn.ksdshpx.test;
+
+/**
+ * Create with IntelliJ IDEA
+ * Create by peng.x
+ * Date: 2017/12/26
+ * Time: 10:05
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!!");
+    }
+}

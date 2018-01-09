7937ecc97591978b0dfaade83f0220480eee22a0
ksdshpx <southeast_px@163.com>
2018-01-09 15:14:42 +0800

    第一个java程序!

---
+++ src/cn/ksdshpx/javase/day01/HelloWorld.java
@@ -0,0 +1,14 @@
+package cn.ksdshpx.javase.day01;
+
+/**
+ * Create with IntelliJ IDEA
+ * Create by peng.x
+ * Date: 2018/1/9
+ * Time: 15:13
+ * Description:第一个java程序
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}

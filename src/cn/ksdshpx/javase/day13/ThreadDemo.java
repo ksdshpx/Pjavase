05767729af2af47bcf7add00fc605f957ac51cb4
ksdshpx <southeast_px@163.com>
2018-05-16 11:06:23 +0800

    创建线程的第二种方式，实现Runnable接口！

---
+++ src/cn/ksdshpx/javase/day13/ThreadDemo.java
@@ -0,0 +1,29 @@
+package cn.ksdshpx.javase.day13;
+
+/**
+ * Create with IntelliJ IDEA
+ * Create by peng.x
+ * Date: 2018/5/16
+ * Time: 10:34
+ * Description:创建线程的第二种方式
+ *              步骤：1，定义类，实现Runnable接口
+ *                   2，覆盖Runnable接口的run()方法
+ *                   3，创建Thread类，将Runnable接口的子类对象作为参数传递给其构造函数
+ *                   4，调用Thread类的start()方法
+ */
+class Demo implements Runnable{
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(Thread.currentThread().getName()+"---"+i);
+        }
+    }
+}
+
+public class ThreadDemo {
+    public static void main(String[] args) {
+        Demo d = new Demo();
+        new Thread(d).start();
+        new Thread(d).start();
+    }
+}

6490efcba8288b9d2b995b300fcc333f52b76a40
ksdshpx <southeast_px@163.com>
2018-07-08 18:27:07 +0800

    集合框架

---
+++ src/cn/ksdshpx/javase/day16/CollectionDemo.java
@@ -0,0 +1,28 @@
+package cn.ksdshpx.javase.day16;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * Create with IntelliJ IDEA
+ * Create by peng.x
+ * Date: 2018/7/8
+ * Time: 14:13
+ * Description:集合框架常用方法
+ */
+public class CollectionDemo {
+    public static void main(String[] args) {
+        Collection coll = new ArrayList();
+        //添加元素
+        coll.add("abc1");
+        coll.add(true);
+        coll.add(123);
+        System.out.println(coll);
+        //删除元素
+        coll.remove(true);
+        System.out.println(coll);
+        //清空集合
+        coll.clear();
+        System.out.println(coll);
+    }
+}
---
+++ src/cn/ksdshpx/javase/day16/WrapperDemo.java
@@ -0,0 +1,27 @@
+package cn.ksdshpx.javase.day16;
+
+/**
+ * Create with IntelliJ IDEA
+ * Create by peng.x
+ * Date: 2018/7/4
+ * Time: 19:48
+ * Description:基本数据类型对象包装类
+ */
+public class WrapperDemo {
+    public static void main(String[] args) {
+        //基本数据类型与String的转换
+        //1.String-->基本数据类型
+        int i = Integer.parseInt("123");
+        System.out.println(i + 3);
+        //基本数据类型-->String
+        String s = String.valueOf(123);
+        System.out.println(s);
+        //进制转换
+        //十进制-->其他进制
+        System.out.println(Integer.toBinaryString(60));
+        System.out.println(Integer.toHexString(60));
+        System.out.println(Integer.toOctalString(60));
+        //其他进制-->十进制
+        System.out.println(Integer.parseInt("3c",16));
+    }
+}

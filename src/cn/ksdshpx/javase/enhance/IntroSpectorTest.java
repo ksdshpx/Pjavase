bdc0c2243fddb0e0e42446987a802dfd862b202a
ksdshpx <southeast_px@163.com>
2018-08-20 10:11:22 +0800

    内省的简单实现！

---
+++ src/cn/ksdshpx/javase/enhance/IntroSpectorTest.java
@@ -0,0 +1,38 @@
+package cn.ksdshpx.javase.enhance;
+
+import java.beans.IntrospectionException;
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+/**
+ * Create with IntelliJ IDEA
+ * Create by peng.x
+ * Date: 2018/8/20
+ * Time: 10:04
+ * Description:内省案例
+ */
+public class IntroSpectorTest {
+    public static void main(String[] args) throws Exception {
+        ReflectPoint rp = new ReflectPoint(3, 5);
+        String propertyName = "x";
+        Object retVal = getProperty(rp, propertyName);
+        System.out.println(retVal);
+        System.out.println("------------------------");
+        int value = 88;
+        setProperty(rp, propertyName, value);
+        System.out.println(rp.getX());
+    }
+
+    private static void setProperty(ReflectPoint rp, String propertyName, int value) throws IntrospectionException, IllegalAccessException, InvocationTargetException {
+        PropertyDescriptor pd2 = new PropertyDescriptor(propertyName, rp.getClass());
+        Method methodSetX = pd2.getWriteMethod();
+        methodSetX.invoke(rp, value);
+    }
+
+    private static Object getProperty(Object rp, String propertyName) throws IntrospectionException, IllegalAccessException, InvocationTargetException {
+        PropertyDescriptor pd1 = new PropertyDescriptor(propertyName, rp.getClass());
+        Method methodGetX = pd1.getReadMethod();
+        return methodGetX.invoke(rp);
+    }
+}

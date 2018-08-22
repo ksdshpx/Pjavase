488ec282107d1e46779c1c15708605fe44ed9671
ksdshpx <southeast_px@163.com>
2018-08-22 12:13:27 +0800

    SQL注入问题！

---
+++ src/cn/ksdshpx/javase/jdbc/SqlInjectTest.java
@@ -0,0 +1,48 @@
+package cn.ksdshpx.javase.jdbc;
+
+import java.sql.*;
+
+/**
+ * Create with IntelliJ IDEA
+ * Create by peng.x
+ * Date: 2018/8/22
+ * Time: 10:48
+ * Description:SQL注入问题
+ */
+public class SqlInjectTest {
+    public static void main(String[] args) {
+        read("' or 1 or'");
+    }
+
+    static void read(String ename) {
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            //得到连接
+            conn = JdbcUtils.getConnection();
+            //创建Statement
+            stmt = conn.createStatement();
+            //通过Statement对象发送sql语句
+            String sql = "select * from emp where ename = '" + ename + "'";
+            System.out.println("sql:" + sql);
+            rs = stmt.executeQuery(sql);
+            ResultSetMetaData metaData = rs.getMetaData();
+            int columnCount = metaData.getColumnCount();
+            for (int i = 1; i <= columnCount; i++) {
+                System.out.print(metaData.getColumnName(i) + "\t");
+            }
+            System.out.println();
+            while (rs.next()) {
+                int empno = rs.getInt("empno");
+                String ename2 = rs.getString("ename");
+                int deptno = rs.getInt("deptno");
+                System.out.println(empno + "\t" + ename2 + "\t" + deptno);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JdbcUtils.close(conn, stmt, rs);
+        }
+    }
+}

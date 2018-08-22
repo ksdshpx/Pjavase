6104f5be94a684783685db3eef4fa5997cb0e402
ksdshpx <southeast_px@163.com>
2018-08-22 13:56:46 +0800

    通过PreparedStatement解决SQL注入问题！

--- src/cn/ksdshpx/javase/jdbc/SQLInject.java
+++ src/cn/ksdshpx/javase/jdbc/SQLInject.java
@@ -9,9 +9,11 @@ import java.sql.*;
  * Time: 10:48
  * Description:SQL注入问题
  */
-public class SqlInjectTest {
+public class SQLInject {
     public static void main(String[] args) {
         read("' or 1 or'");
+        System.out.println("------------------");
+        readFixSQLInject("lisi");
     }
 
     static void read(String ename) {
@@ -43,6 +45,39 @@ public class SqlInjectTest {
             e.printStackTrace();
         } finally {
             JdbcUtils.close(conn, stmt, rs);
+        }
+    }
+
+    static void readFixSQLInject(String ename) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            //得到连接
+            conn = JdbcUtils.getConnection();
+            //创建Statement
+            String sql = "select * from emp where ename = ?";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, ename);
+            System.out.println("sql:" + sql);
+            //通过PreparedStatement对象发送sql语句
+            rs = pstmt.executeQuery();
+            ResultSetMetaData metaData = rs.getMetaData();
+            int columnCount = metaData.getColumnCount();
+            for (int i = 1; i <= columnCount; i++) {
+                System.out.print(metaData.getColumnName(i) + "\t");
+            }
+            System.out.println();
+            while (rs.next()) {
+                int empno = rs.getInt("empno");
+                String ename2 = rs.getString("ename");
+                int deptno = rs.getInt("deptno");
+                System.out.println(empno + "\t" + ename2 + "\t" + deptno);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            JdbcUtils.close(conn, pstmt, rs);
         }
     }
 }

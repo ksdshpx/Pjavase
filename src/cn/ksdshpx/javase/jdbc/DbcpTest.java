3e3deac5c4032f2630a3f1a805f3635b4a257f59
ksdshpx <southeast_px@163.com>
2018-08-26 17:59:18 +0800

    dbcp数据库连接池测试！

---
+++ src/cn/ksdshpx/javase/jdbc/DbcpTest.java
@@ -0,0 +1,38 @@
+package cn.ksdshpx.javase.jdbc;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Create with IntelliJ IDEA
+ * Create by peng.x
+ * Date: 2018/8/26
+ * Time: 17:50
+ * Description:dbcp数据库连接池测试
+ */
+public class DbcpTest {
+    @Test
+    public void test(){
+        try {
+            //1.创建dbcp数据库连接池对象
+            BasicDataSource dataSource = new BasicDataSource();
+            //2.设置四大参数
+            dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+            dataSource.setUrl("jdbc:mysql://localhost:3306/mydb1");
+            dataSource.setUsername("root");
+            dataSource.setPassword("root123");
+            //设置连接池参数
+            dataSource.setMaxIdle(3);//最小空闲连接数
+            dataSource.setInitialSize(10);
+            dataSource.setMaxWait(1000);
+            //4.得到Connection
+            Connection conn = dataSource.getConnection();
+            System.out.println(conn.getClass().getName());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}

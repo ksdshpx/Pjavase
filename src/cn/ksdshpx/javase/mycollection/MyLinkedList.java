bfaf8625ecd679ced20143644a976298bca043d4
ksdshpx <southeast_px@163.com>
2018-10-05 21:50:32 +0800

    添加删除节点方法！

--- src/cn/ksdshpx/javase/mycollection/MyLinkedList.java
+++ src/cn/ksdshpx/javase/mycollection/MyLinkedList.java
@@ -41,9 +41,18 @@ public class MyLinkedList {
     }
 
     public Object get(int index) {
+        checkIndex(index);
+        Node tmp = getNode(index);
+        return tmp != null ? tmp.data : null;
+    }
+
+    private void checkIndex(int index) {
         if (index < 0 || index > size - 1) {
             throw new RuntimeException("索引值不合法:" + index);
         }
+    }
+
+    private Node getNode(int index) {
         Node tmp = null;
         if (index <= (size >> 1)) {
             tmp = first;
@@ -56,7 +65,25 @@ public class MyLinkedList {
                 tmp = tmp.previous;
             }
         }
-        return tmp.data;
+        return tmp;
+    }
+
+    public void remove(int index) {
+        checkIndex(index);
+        Node removeNode = getNode(index);
+        Node preNode = removeNode.previous;
+        Node nextNode = removeNode.next;
+        if (preNode != null) {
+            preNode.next = nextNode;
+        } else {
+            first = nextNode;
+        }
+        if (nextNode != null) {
+            nextNode.previous = preNode;
+        } else {
+            last = preNode;
+        }
+        size--;
     }
 
     @Override
@@ -81,6 +108,12 @@ public class MyLinkedList {
         linkedList.add("e");
         linkedList.add("f");
         System.out.println(linkedList);
-        System.out.println(linkedList.get(6));
+        System.out.println(linkedList.get(5));
+        linkedList.remove(3);
+        System.out.println(linkedList);
+        linkedList.remove(0);
+        System.out.println(linkedList);
+        linkedList.remove(3);
+        System.out.println(linkedList);
     }
 }

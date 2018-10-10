78f299647ed907c9893f352101ab45f0065eab9b
ksdshpx <southeast_px@163.com>
2018-10-10 15:03:15 +0800

    解决put方法键重复问题！

--- src/cn/ksdshpx/javase/mycollection/MyHashMap.java
+++ src/cn/ksdshpx/javase/mycollection/MyHashMap.java
@@ -28,10 +28,24 @@ public class MyHashMap {
         newNode.key = key;
         newNode.value = value;
         Node tmp = table[newNode.hash];
-        if (tmp == null) {
+        Node lastNode = null;
+        boolean keyRepeated = false;
+        if (tmp == null) {//数组元素为空，直接将新节点放进去
             table[newNode.hash] = newNode;
-        } else {
-
+        } else {//数组元素不为空，遍历对应链表
+            while (tmp != null) {
+                if (tmp.key.equals(key)) {//键重复，需要覆盖
+                    keyRepeated = true;
+                    tmp.value = value;
+                    break;
+                } else {//键不重复
+                    lastNode = tmp;
+                    tmp = tmp.next;
+                }
+            }
+            if (!keyRepeated) {
+                lastNode.next = newNode;
+            }
         }
     }
 
@@ -44,6 +58,10 @@ public class MyHashMap {
         hashMap.put(10, "aa");
         hashMap.put(20, "bb");
         hashMap.put(30, "cc");
+        hashMap.put(20, "ddyy");
+        hashMap.put(53, "gg");
+        hashMap.put(69, "ff");
+        hashMap.put(85, "ee");
         System.out.println(hashMap);
     }
 }

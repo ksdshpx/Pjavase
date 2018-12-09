d62c68c2c8839a058a56b0269028c9033a8a0f7b
peng.xing <southeast_px@163.com>
2018-12-09 10:33:26 +0800

    字符缓冲流BufferedWriter!

---
+++ src/cn/ksdshpx/javase/day20/BufferedWriterDemo.java
@@ -0,0 +1,34 @@
+package cn.ksdshpx.javase.day20;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Create with IntelliJ IDEA
+ * Create by peng.x
+ * Date: 2018/12/9
+ * Time: 10:30
+ * Description:字符缓冲流
+ */
+public class BufferedWriterDemo {
+    public static void main(String[] args) {
+        BufferedWriter bufferedWriter = null;
+        try {
+            bufferedWriter = new BufferedWriter(new FileWriter("d:\\buff.txt"));
+            bufferedWriter.write("abcd");
+            bufferedWriter.newLine();
+            bufferedWriter.write("efg");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (bufferedWriter != null) {
+                try {
+                    bufferedWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}

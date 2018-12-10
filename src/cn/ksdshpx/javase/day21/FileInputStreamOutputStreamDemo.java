1c47aa66b07646647a4f099d46ee9de32042bd7f
peng.xing <southeast_px@163.com>
2018-12-10 10:09:50 +0800

    字节流！

---
+++ src/cn/ksdshpx/javase/day21/FileInputStreamOutputStreamDemo.java
@@ -0,0 +1,45 @@
+package cn.ksdshpx.javase.day21;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Create with IntelliJ IDEA
+ * Create by peng.x
+ * Date: 2018/12/10
+ * Time: 10:04
+ * Description:字节流
+ */
+public class FileInputStreamOutputStreamDemo {
+    public static void main(String[] args) {
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+        try {
+            fis = new FileInputStream("d:\\SystemDemo.java");
+            fos = new FileOutputStream("d:\\Copy.java");
+            int len = 0;
+            byte[] buf = new byte[1024];
+            while ((len = fis.read(buf)) != -1) {
+                fos.write(buf);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}

49dc9818a96de92aa245dd24df21f99b29ed0da7
peng.xing <southeast_px@163.com>
2018-12-14 10:32:37 +0800

    文件对象File常用方法之获取方法！

---
+++ src/cn/ksdshpx/javase/day22/FileMethodDemo.java
@@ -0,0 +1,35 @@
+package cn.ksdshpx.javase.day22;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Create with IntelliJ IDEA
+ * Create by peng.x
+ * Date: 2018/12/14
+ * Time: 10:22
+ * Description:文件对象的常用方法
+ */
+public class FileMethodDemo {
+    public static void main(String[] args) {
+        //1.文件对象的获取功能
+        getDemo();
+    }
+
+    public static void getDemo() {
+        File file = new File("D:\\icode\\Pjavase\\src\\cn\\ksdshpx\\javase\\day22\\SystemInDemo.java");
+        //输出文件的名称
+        System.out.println("文件名称：" + file.getName());//SystemInDemo.java
+        //输出文件的绝对路径
+        System.out.println("文件绝对路径：" + file.getAbsolutePath());//D:\icode\Pjavase\src\cn\ksdshpx\javase\day22\SystemInDemo.java
+        //输出文件的相对路径
+        System.out.println("文件相对路径：" + file.getPath());//D:\icode\Pjavase\src\cn\ksdshpx\javase\day22\SystemInDemo.java
+        //输出文件的父目录
+        System.out.println("文件的父目录：" + file.getParent());//D:\icode\Pjavase\src\cn\ksdshpx\javase\day22
+        //输出文件大小
+        System.out.println("文件的大小："+file.length());
+        //输出文件最后修改时间
+        System.out.println("文件最后修改时间："+new SimpleDateFormat("yyyy-MM-dd hh:mm:ss SSS").format(new Date(file.lastModified())));
+    }
+}

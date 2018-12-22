b024af22d11fa439b30a4dc5bca27454603ebad1
peng.xing <southeast_px@163.com>
2018-12-22 11:23:53 +0800

    Properties集合常见方法之store！

--- src/cn/ksdshpx/javase/day23/PropertiesDemo.java
+++ src/cn/ksdshpx/javase/day23/PropertiesDemo.java
@@ -1,5 +1,7 @@
 package cn.ksdshpx.javase.day23;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Properties;
 import java.util.Set;
 
@@ -11,11 +13,25 @@ import java.util.Set;
  * Description:Properties集合的常见方法
  */
 public class PropertiesDemo {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // 存、取、遍历方法
         //demo_1();
         // list方法（调试程序用）
-        demo_2();
+        //demo_2();
+        // store方法
+        demo_3();
+    }
+
+    private static void demo_3() throws IOException {
+        //创建一个Properties集合
+        Properties props = new Properties();
+        //往集合中存入元素（注意Properties中键值都只能是String）
+        props.setProperty("zhangSan", "20");
+        props.setProperty("liSi", "25");
+        props.setProperty("wangWu", "18");
+        props.setProperty("zhaoLiu", "35");
+        FileOutputStream outputStream = new FileOutputStream("D:\\info.txt");
+        props.store(outputStream, "name+age");
     }
 
     private static void demo_2() {

47dfb0a0af49d72d6979c31fb04ed8b180ee4d23
peng.xing <southeast_px@163.com>
2018-12-22 10:49:07 +0800

    Properties集合常见方法之存取遍历！

---
+++ src/cn/ksdshpx/javase/day23/PropertiesDemo.java
@@ -0,0 +1,34 @@
+package cn.ksdshpx.javase.day23;
+
+import java.util.Properties;
+import java.util.Set;
+
+/**
+ * Create with IntelliJ IDEA
+ * Create by peng.x
+ * Date: 2018/12/22
+ * Time: 10:44
+ * Description:Properties集合的常见方法
+ */
+public class PropertiesDemo {
+    public static void main(String[] args) {
+        // 存、取、遍历方法
+        demo_1();
+    }
+
+    private static void demo_1() {
+        //创建一个Properties集合
+        Properties props = new Properties();
+        //往集合中存入元素（注意Properties中键值都只能是String）
+        props.setProperty("zhangSan", "20");
+        props.setProperty("liSi", "25");
+        props.setProperty("wangWu", "18");
+        props.setProperty("zhaoLiu", "35");
+        //遍历集合
+        Set<String> names = props.stringPropertyNames();
+        for (String name : names) {
+            String value = props.getProperty(name);
+            System.out.println(name + ":" + value);
+        }
+    }
+}

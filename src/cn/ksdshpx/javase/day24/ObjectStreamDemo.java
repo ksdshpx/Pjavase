629bd0a32c2caf2b54a80f3090b2cc6ba39bc7d0
peng.xing <southeast_px@163.com>
2019-02-13 16:31:06 +0800

    对象的反序列化！

--- src/cn/ksdshpx/javase/day24/ObjectStreamDemo.java
+++ src/cn/ksdshpx/javase/day24/ObjectStreamDemo.java
@@ -1,20 +1,25 @@
 package cn.ksdshpx.javase.day24;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
 
 /**
  * Create with IntelliJ IDEA
  * Create by peng.x
  * Date: 2019/2/13
  * Time: 15:26
- * Description:对象的序列化
+ * Description:对象的序列化以及反序列化
  *             注意：被序列化的对象必须实现Serializable接口
  */
 public class ObjectStreamDemo {
-    public static void main(String[] args) throws IOException{
-        writeObj();
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        //writeObj();
+        readObj();
+    }
+
+    public static void readObj() throws IOException, ClassNotFoundException {
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("d:\\my.object"));
+        Person p = (Person) ois.readObject();
+        System.out.println(p.getName()+":"+p.getAge());
     }
 
     public static void writeObj() throws IOException {

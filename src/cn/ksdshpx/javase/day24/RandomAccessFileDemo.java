bacefa59996938c4aa6994f0f323de2f6723efb2
peng.xing <southeast_px@163.com>
2019-02-15 11:00:24 +0800

    RandomAccessFile读取以及随机读取！

--- src/cn/ksdshpx/javase/day24/RandomAccessFileDemo.java
+++ src/cn/ksdshpx/javase/day24/RandomAccessFileDemo.java
@@ -17,7 +17,22 @@ import java.io.RandomAccessFile;
  */
 public class RandomAccessFileDemo {
     public static void main(String[] args) throws IOException {
-        writeFile();
+        //writeFile();
+        readFile();
+    }
+
+    public static void readFile() throws IOException {
+        RandomAccessFile raf = new RandomAccessFile("d:\\ranacc.txt","r");
+        //通过seek设置指针的位置
+        raf.seek(10);
+        byte[] buf = new byte[6];
+        raf.read(buf);
+        String name = new String(buf);
+        System.out.println("name:"+name);
+        int age = raf.readInt();
+        System.out.println("age:"+age);
+        System.out.println("pos:"+raf.getFilePointer());
+        raf.close();
     }
 
     //使用RandomAccessFile对象写入一些人员信息，比如姓名和年龄
@@ -25,6 +40,8 @@ public class RandomAccessFileDemo {
         RandomAccessFile raf = new RandomAccessFile("d:\\ranacc.txt","rw");
         raf.write("张三".getBytes());
         raf.writeInt(97);
+        raf.write("小强".getBytes());
+        raf.writeInt(99);
         raf.close();
     }
 }
